2ae9643b7cd8554ed4998691c14181b8c3e5bed2
Alban Farchi <alban.farchi@enpc.fr>
2026-01-13 12:16:28 +0100

    initialisation also writes output shape

--- src/cpp_torch/init_export.py
+++ src/cpp_torch/init_export.py
@@ -28,3 +28,12 @@ def init_and_export(name, batch_size):
         f.write(f'{len(input_shape)}')
         for i in input_shape:
             f.write(f'\n{i}')
+
+    output_shape = (batch_size, *model.output_shape)
+    filename = wdir / 'output_shape.txt'
+    logger.info(f'saving output shape into "{filename}"')
+    with open(filename, 'w') as f:
+        f.write(f'{name}\n')
+        f.write(f'{len(output_shape)}')
+        for i in output_shape:
+            f.write(f'\n{i}')
